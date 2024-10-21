b79d1ae61aaf8d6d6b63654ea07dd7d1ee9334ff
Fernanflop091o <162623537+Fernanflop091o@users.noreply.github.com>
2024-10-21 04:51:37 -0500

    Update Auto3.lua

--- Auto3.lua
+++ Auto3.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Fernanflop091o/Prueba/refs/heads/main/Auto2.lua?token=GHSAT0AAAAAACWQNCNAWAUBS6GZUBCHOI3GZYWENOQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Fernanflop091o/Prueba/refs/heads/main/Auto2.lua"))()
